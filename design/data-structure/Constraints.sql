4a1c243e1935ac2312e98f44e8a4354e356b9921
lawrencekoehler <lawrence.kohler@student.fontys.nl>
2020-10-11 23:05:19 +0200

    database & constraint script

---
+++ design/data-structure/Constraints.sql
@@ -0,0 +1,19 @@
+alter table users add constraint fk_users_roles foreign key (user_role) references user_role (role_id);
+
+alter table users add constraint fk_users_cc foreign key (cc_number) references credit_card (cc_number);
+
+alter table album add constraint fk_album_users foreign key (artist_id) references users (user_id);
+
+alter table playlist add constraint fk_playlist_users foreign key (user_id) references users (user_id);
+
+alter table song add constraint fk_song_user foreign key (artist_id) references users (user_id),
+add constraint fk_song_album foreign key (album_id) references album (album_id);
+
+alter table permissions add constraint fk_permissions_user_role foreign key (user_role_id) references user_role (role_id);
+
+alter table bill add constraint fk_bill_users foreign key (user_id) references users (user_id);
+
+alter table giftcard add constraint fk_giftcard_users foreign key (user_id) references users (user_id);
+
+alter table song_playlist add constraint fk_song_playlist_song foreign key (song_id) references song (song_id),
+add constraint fk_song_playlist_playlist foreign key (playlist_id) references playlist (playlist_id);
---
+++ design/data-structure/DatabaseScript.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE SONGIFY;
+CREATE TABLE Users (
+	User_ID serial primary key,
+	First_Name varchar(255) not null,
+	Last_Name varchar(255) not null,
+	Date_Of_Birth date not null,
+	Email varchar(50) unique,
+	Password varchar(32) not null,
+	User_Name varchar(20) not null,
+	Address varchar(255) not null,
+	Blocked boolean default false,
+	Subscription_Status boolean default false,
+	User_Role int,
+	CC_Number varchar(25)	 
+);
+CREATE TABLE Album (
+	Album_ID serial primary key,
+	Title varchar(50) not null,
+	Date date,
+	Artist_ID serial ,
+	Label varchar(50),
+	Publisher varchar(50)
+);
+CREATE TABLE Song (
+	Song_ID serial primary key,
+	Title varchar(50) not null,
+	Date date,
+	Artist_ID int,
+	Label varchar(50),
+	Publisher varchar(50),
+	Price money,
+	Listens int,
+	Album_ID serial 
+);
+CREATE TABLE Playlist (
+	Playlist_ID serial  primary key,
+	Name varchar(50) not null,
+	User_ID int,
+	Public boolean default false
+);
+CREATE TABLE Song_Playlist (
+	Playlist_ID serial ,
+	Song_ID serial 
+);
+CREATE TABLE Permissions ( 
+	Manage_Users boolean default false,
+	Upload_Songs boolean default false,
+	Listen_To_Songs boolean default false,
+	Create_Playlist boolean default false,
+	Create_Albums boolean default false,
+	User_Role_ID serial  primary key
+);
+CREATE TABLE User_Role (
+	Role_ID serial  primary key,
+	Role_Name varchar(15) not null,
+	Role_Description varchar(50)
+);
+CREATE TABLE Credit_Card (
+	CC_Number varchar(25) primary key,
+	CVC_CVV_Code char(3) not null,
+	Type_Of_Card varchar(50) not null,
+	First_Name varchar(255) not null,
+	Last_Name varchar(255) not null,
+	Expiration_Date date not null
+);
+CREATE TYPE payment AS enum ('Credit Card','Giftcard');
+CREATE TABLE Bill ( 
+	Bill_ID serial  primary key,
+	Date date not null,
+	Price money not null,
+	Status boolean default false,
+	Payment_Method payment,
+	User_ID int
+);
+CREATE TABLE Giftcard (
+	Code char(12) primary key,
+	Value int not null,
+	Status boolean default false,
+	User_ID serial
+);
