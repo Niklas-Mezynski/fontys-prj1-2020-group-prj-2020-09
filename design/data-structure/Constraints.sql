77a1f2e011b2ef6086d43127b850522d1c7e33bb
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2020-10-22 18:31:55 +0200

    Update Constraints.sql

--- design/data-structure/Constraints.sql
+++ design/data-structure/Constraints.sql
@@ -15,8 +15,8 @@ alter table bill add constraint fk_bill_users foreign key (user_id) references u
 
 alter table giftcard add constraint fk_giftcard_users foreign key (user_id) references users (user_id);
 
-alter table song_playlist add constraint fk_song_playlist_song foreign key (song_id) references song (song_id),
-add constraint fk_song_playlist_playlist foreign key (playlist_id) references playlist (playlist_id);
+alter table playlist_songs add constraint fk_song_id_to_song foreign key (song_id) references song (song_id),
+add constraint fk_playlist_id_to_playlist foreign key (playlist_id) references playlist (playlist_id);
 
 alter table song add constraint legal_prices
 check ((Price >= 0.10) and (Price <= 3.00));
