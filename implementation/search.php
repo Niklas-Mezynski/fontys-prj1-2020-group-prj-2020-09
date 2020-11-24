e96206de94ec42adc9a20ab566c2d94fe9b0e1f2
Max0004 <46004466+Max0004@users.noreply.github.com>
2020-11-24 13:50:09 +0100

    save space

--- implementation/search.php
+++ implementation/search.php
@@ -33,23 +33,7 @@
             </nav><!-- end of nav -->
         </aside>
         <?php 
-            $host = "prj1_postgres";
-            $port = "5432";
-            $db = "postgres";   /*Just for now*/
-            $user = "postgres";
-            $pword = "mypassword";
-              
-            $dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pword";
-              
-            try{
-                $conn = new PDO($dsn);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                if($conn){
-                    // ...
-                }
-            }catch(PDOException $e){
-                echo $e->getMessage();
-            }
+            include_once ("dbconnection.php");
         ?>
         <article>
             <!-- actual search page content -->
