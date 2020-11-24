3284b50f7b460456ee59d698750928d850eae959
Roy <70884640+roybw@users.noreply.github.com>
2020-11-24 13:51:45 +0100

    move dbconnection

---
+++ implementation/php/dbconnection.php
@@ -0,0 +1,19 @@
+<?php
+    $host = "prj1_postgres";
+    $port = "5432";
+    $db = "postgres";   /*Just for now*/
+    $user = "postgres";
+    $pword = "mypassword";
+
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pword";
+
+    try{
+        $conn = new PDO($dsn);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if($conn){
+            echo "Connection to the $db database successfully";
+        }
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+?>
