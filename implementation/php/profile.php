651aca2b614309086f4dd9bc30f00b0949c4454f
Felix172 <f.pralle@student.fontys.nl>
2020-11-27 16:39:18 +0100

    .

--- implementation/php/profile.php
+++ implementation/php/profile.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+if (!(isset($_SESSION["user_id"]) && isset($_SESSION["user_id"]))) {
+	header("Location: home.php");
+	session_destroy();
+	exit;
+}
+if ($_SESSION["user_role"] < 1) {
+	header("Location: main.php");
+	exit;
+}
+?>
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -9,27 +22,45 @@
 
 <body>
 
-    <main>
-        <header>
-            <div id="logo"><img id="logo" src="../img/Logo.png" alt="Songify" width="60" height="60" style="display: inline-block; ;"></div>
-            <div id="profileButton"><a href="profile.php">User Profile</a></div>
-        </header><!-- end of header -->
+	<main>
+		<header>
+			<div id="logo"><img id="logo" src="../img/Logo.png" alt="Songify" width="60" height="60" style="display: inline-block; ;"></div>
+			<div id="profileButton"><a href="profile.php">User Profile</a></div>
+			<div id="title">
+				<p>Songify</p>
+			</div>
+		</header><!-- end of header -->
 
-        <aside>
-            <nav id="menu_v">
-                <form action="search.php" method="POST">
-                    <input type="text" name="search" placeholder="Search.." id="searchbar">
-                </form>
-                <ul>
-                    <li><a href="main.php">Home</a></li>
-                    <li><a href="library.php">Library</a></li>
-                    <li><a href="playlists.php">Playlists</a></li>
-                    <li><a href="shop.php">Shop</a></li>
-                    <li><a href="trends.php">Trends</a></li>
-                    <li><a href="logout.php">Logout</a></li>
-                </ul>
-            </nav><!-- end of nav -->
-        </aside>
+		<aside>
+			<nav id="menu_v">
+				<form action="search.php" method="POST">
+					<input type="text" name="search" placeholder="Search.." id="searchbar">
+				</form>
+				<ul>
+					<li><a href="main.php">Home</a></li>
+					<li><a href="library.php">Library</a></li>
+					<?php
+					if ($_SESSION["user_role"] >= 2) {
+						echo '<li><a href="playlists.php">Playlists</a></li>';
+					}
+					?>
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="trends.php">Trends</a></li>
+					<?php
+					if ($_SESSION["user_role"] >= 3) {
+						echo '<li><a href="uploadsongs.php">Upload Songs</a></li>';
+					}
+					?>
+					<?php
+					if ($_SESSION["user_role"] == 4) {
+						echo '<li><a href="admin.php">Admin Panel</a></li>';
+					}
+					?>
+					<li><a href="logout.php">Logout</a></li>
+
+				</ul>
+			</nav><!-- end of nav -->
+		</aside>
 
         <article>
             <div>
@@ -40,8 +71,11 @@
                 require("dbconnection.php");
                 $sqlconnect = "select First_Name, Last_Name, date_of_birth, email, password
                                 from users u 
-                                where user_id = 6";
-                $stmt = $conn -> query($sqlconnect);
+                                where user_id = :user_id";
+                
+                $stmt = $conn -> prepare($sqlconnect);
+                $stmt -> bindParam(":user_id", $_SESSION['user_id']);
+                $stmt -> execute();
                 foreach($stmt as $row) {
                     echo "<li>First Name: " . $row ['first_name'] . 
                         "<li> Last Name: " . $row ['last_name'] . 
@@ -79,7 +113,6 @@
             <div id="sub">
                 <p>Subscription End Date: DD-MM-YYYY</p>
             </div>
-            <br><a href="logout.php">Logout</a>
 
         </article>
 
