4e3beacc23b4e250f150a9f24d36336d0349ada2
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2020-11-27 13:26:24 +0100

    .

--- implementation/php/shop.php
+++ implementation/php/shop.php
@@ -4,31 +4,31 @@
 <head>
 	<title>Home</title>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="css/main.css"><!-- link to stylesheet -->
-	<link rel="stylesheet" href="css/shop.css">
+	<link rel="stylesheet" href="../css/main.css"><!-- link to stylesheet -->
+	<link rel="stylesheet" href="../css/shop.css">
 </head>
 
 <body>
 
 	<main>
-		<header>
-			<div id="logo"><img id="logo" src="img/Logo.png" alt="Songify" width="60" height="60" style="display: inline-block; ;"></div>
-			<div id="profileButton"><a href="profile.html">User Profile</a></div>
+	<header>
+			<div id="logo"><img id="logo" src="../img/Logo.png" alt="Songify" width="60" height="60" style="display: inline-block; ;"></div>
+			<div id="profileButton"><a href="profile.php">User Profile</a></div>
 			<div id="title"><p>Songify</p></div>
 		</header><!-- end of header -->
 
 		<aside>
 			<nav id="menu_v">
-				<form action="search.php" method="GET">
+				<form action="search.php" method="POST">
 					<input type="text" name="search" placeholder="Search.." id="searchbar">
 				</form>
 				<ul>
-					<li><a href="home.html">Home</a></li>
-					<li><a href="library.html">Library</a></li>
-					<li><a href="playlists.html">Playlists</a></li>
-					<li><a href="shop.html">Shop</a></li>
-					<li><a href="trends.html">Trends</a></li>
-					<li><a href="login.html">Logout</a></li>
+					<li><a href="main.php">Home</a></li>
+					<li><a href="library.php">Library</a></li>
+					<li><a href="playlists.php">Playlists</a></li>
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="trends.php">Trends</a></li>
+					<li><a href="logout.php">Logout</a></li>
 				</ul>
 			</nav><!-- end of nav -->
 		</aside>
@@ -36,14 +36,14 @@
 		<article>
             <div id="shop">
 				<p class="text">Subscription</p>
-				<img src="img/shopitem-placeholder.jpg" class="image"></p>
+				<img src="../img/shopitem-placeholder.jpg" class="image"></p>
 				<button>Purchase</button>
             </div>
 		</article><!-- end of article -->
 
 		<footer>
 			<p>
-				<a href="termsandconditions.html">Terms and Conditions</a>
+				<a href="../termsandconditions.html">Terms and Conditions</a>
 			</p>
 
 		</footer><!-- end of footer -->
