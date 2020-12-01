ef28d7718ab50bc5420efa9113db5fa07143b0ba
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2020-12-01 15:26:49 +0100

    .

--- implementation/php/upload.php
+++ implementation/php/upload.php
@@ -49,7 +49,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $finalFile)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo "The file has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
