954f64a7e4d7c9b27902f2821fa2582f1849321e
Roy <70884640+roybw@users.noreply.github.com>
2020-12-01 15:10:44 +0100

    links

--- implementation/php/trends.php
+++ implementation/php/trends.php
@@ -70,7 +70,7 @@
 
         <footer>
             <p>
-                <a href="termsandconditions.html">Terms and Conditions</a>
+                <a href="../termsandconditions.html">Terms and Conditions</a>
             </p>
 
         </footer><!-- end of footer -->
