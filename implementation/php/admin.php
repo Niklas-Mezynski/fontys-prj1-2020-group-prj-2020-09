44327f709497f4fc850fce78a3c809a72e3841f3
Max0004 <46004466+Max0004@users.noreply.github.com>
2020-12-08 14:40:58 +0100

    lowercase and comments

--- implementation/php/admin.php
+++ implementation/php/admin.php
@@ -20,14 +20,13 @@ if ($_SESSION["user_role"] < 4) {
 </head>
 <body>
 <main>
-		<header>
-			<div id="logo"><img id="logo" src="../img/Logo.png" alt="Songify" width="60" height="60" style="display: inline-block; ;"></div>
-			<div id="profileButton"><a href="profile.php">User Profile</a></div>
-			<div id="title">
-				<p>Songify</p>
-			</div>
+	<header>
+		<div id="logo"><img id="logo" src="../img/Logo.png" alt="Songify" width="60" height="60" style="display: inline-block; ;"></div>
+		<div id="profileButton"><a href="profile.php">User Profile</a></div>
+		<div id="title">
+			<p>Songify</p>
+		</div>
 		</header><!-- end of header -->
-
 		<aside>
 			<nav id="menu_v">
 				<form action="search.php" method="POST">
@@ -38,27 +37,25 @@ if ($_SESSION["user_role"] < 4) {
 					<li><a href="library.php">Library</a></li>
 					<?php
 					if ($_SESSION["user_role"] >= 2) {
-						echo '<li><a href="playlists.php">Playlists</a></li>';
+						echo '<li><a href="playlists.php">Playlists</a></li>';	//If the user_role is Registered or higher, this element will be avaiable
 					}
 					?>
 					<li><a href="shop.php">Shop</a></li>
 					<li><a href="trends.php">Trends</a></li>
 					<?php
 					if ($_SESSION["user_role"] >= 3) {
-						echo '<li><a href="uploadsongs.php">Upload Songs</a></li>';
+						echo '<li><a href="uploadsongs.php">Upload Songs</a></li>'; //If the user_role is Artist or Admin, this element will be avaiable
 					}
 					?>
 					<?php
 					if ($_SESSION["user_role"] == 4) {
-						echo '<li><a href="admin.php">Admin Panel</a></li>';
+						echo '<li><a href="admin.php">Admin Panel</a></li>'; //if the user_role is Admin, this element will be avaiable
 					}
 					?>
 					<li><a href="logout.php">Logout</a></li>
-
 				</ul>
 			</nav><!-- end of nav -->
 		</aside>
-
         <article>
             <div id="contentbox">
                 <h1>Admin Interface</h1>
@@ -69,37 +66,37 @@ if ($_SESSION["user_role"] < 4) {
                 </form>
 				<ul>
 					<?php
-						include_once("dbconnection.php");
-						if($_POST["user_search"] != "" || $_POST["user_search"] != null){
-							$search = $_POST["user_search"];
-							if($_POST["searchRadio"] == "Artist"){
+						include_once("dbconnection.php");									//This preloads functions and variables in dbconnection.php
+						if($_POST["user_search"] != "" || $_POST["user_search"] != null){	//Checks if the searchbar is not empty. If it is not empty...
+							$search = strtolower( $_POST["user_search"]);						//...than the searched item will be rewritten in lowercase
+							if($_POST["searchRadio"] == "Artist"){								//Checks if the Filter for the search is Artist. If it is ...
 								$sql = "select user_id,user_name from users
 								where user_role = 3
-								and user_name LIKE '%" . $search . "%'
+								and lower(user_name) LIKE '%" . $search . "%'
 								order by user_name;";
 								$stmt = $conn->prepare($sql);
-								$stmt->execute();
+								$stmt->execute();													//...then the program looks for users who are artists AND have the searched name
 								foreach($stmt as $row){
 									echo "<li><a href='profile.php?id=" . $row["user_id"] . "'>" . $row["user_name"] . "</a></li>";
 								}
-							}else{
+							}else{																	//...otherwise...
 								$sql = "select user_id,user_name from users
 								where user_role != 4
-								and user_name LIKE '%" . $search . "%'
+								and lower(user_name) LIKE '%" . $search . "%'
 								order by user_name;";
 								$stmt = $conn->prepare($sql);
-								$stmt->execute();
+								$stmt->execute();													// the program looks for users with the searched name
 								foreach($stmt as $row){
 									echo "<li><a href='profile.php?id=" . $row["user_id"] . "'>" . $row["user_name"] . "</a></li>";
 								}
 							}
-						}else{
-							if($_POST["searchRadio"] == "Artist"){
+						}else{														//If the Searchbar is empty...
+							if($_POST["searchRadio"] == "Artist"){						//...Than it does the process from ll.72-91. Except without the searched user
 								$sql = "select user_id,user_name from users
 								where user_role = 3
 								order by user_name;";
 								$stmt = $conn->prepare($sql);
-								$stmt->execute();
+								$stmt->execute();										//This query lists all Artists
 								foreach($stmt as $row){
 									echo "<li><a href='profile.php?id=" . $row["user_id"] . "'>" . $row["user_name"] . "</a></li>";
 								}
@@ -108,7 +105,7 @@ if ($_SESSION["user_role"] < 4) {
 								where user_role != 4
 								order by user_name;";
 								$stmt = $conn->prepare($sql);
-								$stmt->execute();
+								$stmt->execute();										//This query lists evry user except oneself
 								foreach($stmt as $row){
 									echo "<li><a href='profile.php?id=" . $row["user_id"] . "'>" . $row["user_name"] . "</a></li>";
 								}
@@ -118,7 +115,6 @@ if ($_SESSION["user_role"] < 4) {
 				</ul>
             </div>
         </article>
-
         <footer>
             <p>
                 <a href="termsandconditions.html">Terms and Conditions</a>
